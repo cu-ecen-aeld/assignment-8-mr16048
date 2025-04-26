f298283914e9b7d44ee98e68b5451a0390fa5660
Ryosuke Mori <moriryosuke48@gmail.com>
2025-04-26 10:03:48 +0900

    changed ref to aesdsocket2

--- base_external/package/aesd-socket/aesd-socket.mk
+++ base_external/package/aesd-socket/aesd-socket.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_SOCKET_VERSION = fc9c02beccafb03830701b109a9529a92cfd65b6
+AESD_SOCKET_VERSION = 68216dc06922c8b847cde05cbf61aa502f3ed245
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
