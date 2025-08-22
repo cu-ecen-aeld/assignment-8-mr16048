8c0a60431beada235722730e032560f55d6ee3c7
rmori <moriryosuke48@gmail.com>
2025-08-23 06:23:02 +0900

    add explicit  O option

--- build.sh
+++ build.sh
@@ -35,7 +35,7 @@ then
 	if [ -e ${AESD_MODIFIED_DEFCONFIG} ]
 	then
 		echo "USING ${AESD_MODIFIED_DEFCONFIG}"
-		make -C buildroot defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} BR2_DEFCONFIG=${AESD_MODIFIED_DEFCONFIG_REL_BUILDROOT} V=1
+		make -C buildroot defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} BR2_DEFCONFIG=${AESD_MODIFIED_DEFCONFIG_REL_BUILDROOT} V=1 O=output
 	else
 		echo "Run ./save_config.sh to save this as the default configuration in ${AESD_MODIFIED_DEFCONFIG}"
 		echo "Then add packages as needed to complete the installation, re-running ./save_config.sh as needed"
@@ -44,7 +44,7 @@ then
 else
 	echo "USING EXISTING BUILDROOT CONFIG"
 	echo "To force update, delete .config or make changes using make menuconfig and build again."
-	make -C buildroot BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} V=1 source
+	make -C buildroot BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} V=1 O=output source
 	make -C buildroot BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} V=1
 
 fi
